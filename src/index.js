bce12b82f79d780f240660a383ad66b035148caa
Alberto <albertonietopeg@upm.es>
2022-05-19 01:14:31 +0200

    Configuración intermedia de eslint sin semicolon

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { default as CustomAppBar } from './components/layout/CustomAppBar';
-export { default as CustomAppFooter } from './components/layout/CustomAppFooter';
-export { default as LayoutProvider } from './providers/LayoutProvider';
-export { useLayout } from './hooks/useLayout';
+export { default as CustomAppBar } from './components/layout/CustomAppBar'
+export { default as CustomAppFooter } from './components/layout/CustomAppFooter'
+export { default as LayoutProvider } from './providers/LayoutProvider'
+export { useLayout } from './hooks/useLayout'
